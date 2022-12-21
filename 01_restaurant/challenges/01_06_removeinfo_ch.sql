6ce7bff8c9f2777bee3eaaa1f46275b33b7e166c
vojajokic <55879745+vojajokic@users.noreply.github.com>
2022-12-21 11:20:03 +0000

    End of 1.7

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,14 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+UPDATE Reservations SET Date = NULL WHERE ReservationID IN
+(SELECT Reservations.ReservationID
+FROM Reservations
+JOIN Customers ON Reservations.CustomerID=Customers.CustomerID
+WHERE Customers.FirstName='Norby' AND Reservations.Date > '2022-07-24')
+-- Obratiti pažnju da može da postoji više termina za klijenta u budućnosti (treba brisati samo jedan!!!)
+SELECT *
+FROM Reservations
+JOIN Customers ON Reservations.CustomerID=Customers.CustomerID
+WHERE Customers.FirstName='Norby' 
--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,10 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+INSERT INTO AnniversaryAttendees (CustomerID, PartySize)
+VALUES ((SELECT CustomerID FROM Customers WHERE Email = 'atapley2j@kinetecoinc.com'), '3');
+
+SELECT CustomerID
+FROM Customers
+WHERE Email = 'atapley2j@kinetecoinc.com'
