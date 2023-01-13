ea25aa386812225cb27ddd298864e97c00102854
vojajokic <55879745+vojajokic@users.noreply.github.com>
2023-01-13 11:23:50 +0000

    Report of books due to be returned on one date

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,9 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+SELECT Loans.DueDate, Books.Title, Patrons.FirstName, Patrons.Email
+FROM Loans
+JOIN Books ON Loans.BookID = Books.BookID
+JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+WHERE Loans.DueDate = '2022-07-13' AND Loans.ReturnedDate IS NULL;
