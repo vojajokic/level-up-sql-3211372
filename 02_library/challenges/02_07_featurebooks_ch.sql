2abeee93ba214e1475858f1b6824362576b0476c
vojajokic <55879745+vojajokic@users.noreply.github.com>
2023-01-13 13:47:50 +0000

    List of available books published from 1890-1899

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -2,3 +2,12 @@
 
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+
+SELECT Title, Barcode
+FROM Books
+WHERE (Published BETWEEN 1890 AND 1899) 
+  AND (BookID NOT IN 
+    (SELECT BookID
+      FROM Loans 
+      WHERE ReturnedDate IS NULL))
+ORDER BY Title;
