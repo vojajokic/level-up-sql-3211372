a96809378fa963f884c4327a031fb63dba94c0b6
vojajokic <55879745+vojajokic@users.noreply.github.com>
2023-01-13 11:42:17 +0000

    Return Three books to the library

--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -4,3 +4,29 @@
 -- Book 1: 6435968624
 -- Book 2: 5677520613
 -- Book 3: 8730298424
+
+SELECT * 
+FROM Loans
+JOIN Books ON Books.BookID = Loans.BookID
+WHERE Books.Barcode IN ('6435968624','5677520613','8730298424');
+
+UPDATE Loans 
+SET ReturnedDate = '2022-07-05' 
+WHERE BookID = (
+  SELECT BookID 
+  FROM Books
+  WHERE Barcode = '6435968624' AND ReturnedDate IS NULL);
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05' 
+WHERE BookID = (
+  SELECT BookID 
+  FROM Books
+  WHERE Barcode = '5677520613' AND ReturnedDate IS NULL);
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05' 
+WHERE BookID = (
+  SELECT BookID
+  FROM Books
+  WHERE Barcode = '8730298424' AND ReturnedDate IS NULL);
