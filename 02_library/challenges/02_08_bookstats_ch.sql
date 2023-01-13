15ba7d1a6278684585e9b92684cece5ed39e403f
vojajokic <55879745+vojajokic@users.noreply.github.com>
2023-01-13 14:36:07 +0000

    Create Reports

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,16 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+SELECT Published, COUNT(DISTINCT(Title)) AS PubCount, 
+  GROUP_CONCAT(DISTINCT(Title))
+FROM Books
+GROUP BY Published
+ORDER BY PubCount DESC;
+
+SELECT Books.Title, COUNT(Loans.LoanID) AS LoanCount
+FROM Loans
+JOIN Books ON Loans.BookID=Books.BookID
+GROUP BY Books.Title
+ORDER BY LoanCount DESC
+Limit 5;
